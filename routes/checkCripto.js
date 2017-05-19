55c32fc83bb4f224e4e55a948b00203fc92c58b3
Joan Perez <jperezes@cisco.com>
2017-05-19 16:33:58 +0200

    nucky check cripto

---
+++ routes/checkCripto.js
@@ -0,0 +1,39 @@
+let schedule = require('node-schedule');
+let Promise= require('bluebird')
+let rp = require('request-promise');
+let bodyParser = require('body-parser');
+
+let requestCurrencies = function(){};
+// Set the headers
+let headers = {
+    'Content-Type': 'application/json'
+}
+// Configure the request
+let options = {
+    url: "https://api.coinmarketcap.com/v1/ticker/Ripple?convert=EUR",
+    method: 'GET'
+}
+
+requestCurrencies.prototype.scheduleServer = function(bot){
+  schedule.scheduleJob('30 * * * * *', Promise.coroutine(function* () {
+      let message ="Hello Joan last hour criptocurrency update:";
+      let tempMessage="";
+
+      result = yield rp({url:"https://api.coinmarketcap.com/v1/ticker/?&limit=15",method:'GET'});
+      //resultRipple = resultRipple.replace("[","").replace("]","")
+      result = JSON.parse(result)
+      result.forEach(item =>{
+      //  console.log("24h " + item.name + " -> " + item.percent_change_24h + "%");
+        tempMessage = "\n\n >24h " + item.name + " -> " + item.percent_change_24h + "%";
+        message = message + tempMessage;
+      })
+
+      // bot.sendRichTextMessage(roomId,message,function(){
+      //   console.log("user found about to send him a message");
+      // });
+
+      return;
+    }));
+}
+
+module.exports = requestCurrencies;
---
+++ server.js
@@ -0,0 +1,39 @@
+let port = process.env.PORT || 1337;
+let sparklite = require("sparklite");
+let botdomain = process.env.DOLORES_URL;
+let sparkBot = new sparklite.SparkBot(process.env.NUCKY_KEY, port, botdomain);
+let CheckCurrency = require('./routes/checkCripto');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+
+sparkBot.printHelloWorld();
+
+let checkCurrency = new CheckCurrency()
+sparkBot.on('message', function (event)
+{
+   var mail = event.personEmail.split('@');
+
+   if (mail[1] === "cisco.com"){
+     let message = "your room id is : " + event.roomId;
+     sparkBot.sendMessage(event.roomId, message ,function(){});
+   } else {
+
+     sparkBot.sendMessage(event.roomId, "Hi, sorry to tell you that but you're not allowed to proceed",function(){});
+   }
+  console.log(JSON.stringify(event));
+
+})
+sparkBot.on('rooms', function (event)
+{
+    console.log(JSON.stringify(event));
+})
+
+sparkBot.on('memberships', function (event)
+{
+    console.log(JSON.stringify(event));
+})
+
+let bot = function(result){
+  console.log(result);
+}
+
+checkCurrency.scheduleServer(bot);
