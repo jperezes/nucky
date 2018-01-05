c7f167f1c0d744516c6df878489b827828524d59
Joan Perez <jperezes@cisco.com>
2018-01-05 02:19:20 +0100

    try some apis

--- routes/checkCripto.js
+++ routes/checkCripto.js
@@ -15,17 +15,81 @@ let options = {
     url: "https://api.coinmarketcap.com/v1/ticker/Ripple?convert=EUR",
     method: 'GET'
 }
-
+let trend ={
+  "Bitcoin":[String],
+  "Ripple":[String],
+  "Ethereum":[String],
+  "NEM":[String],
+  "Litecoin":[String],
+  "Dash":[String],
+  "Ethereum Classic":[String],
+  "Stellar Lumens":[String],
+  "Monero":[String],
+  "Bytecoin":[String],
+  "Siacoin":[String],
+  "Golem":[String],
+  "Steem":[String],
+  "BitShares":[String],
+  "Dogecoin":[String],
+  "Gnosis":[String],
+  "Stratis":[String],
+  "Augur":[String],
+  "GameCredits":[String],
+  "Waves":[String],
+  "MaidSafeCoin":[String],
+  "Zcash":[String],
+  "DigixDAO":[String],
+  "DigiByte":[String],
+  "Ardor":[String],
+  "SingularDTV":[String],
+  "Round":[String],
+  "Decred":[String],
+  "Iconomi":[String],
+  "Factom":[String],
+  "Lisk":[String],
+  "BitConnect":[String],
+  "Nxt":[String],
+  "Tether":[String],
+  "FirstBlood":[String],
+  "SysCoin":[String],
+  "PIVX":[String],
+  "Byteball":[String],
+  "Peercoin":[String],
+  "Aragon":[String],
+  "Komodo":[String],
+  "Emercoin":[String],
+  "AntShares":[String],
+  "iExec RLC":[String],
+  "Melon":[String],
+  "Namecoin":[String],
+  "Storjcoin X":[String],
+  "Lykke":[String],
+  "BitcoinDark":[String],
+  "Counterparty":[String],
+  "Gulden":[String],
+  "NAV Coin":[String],
+  "YbCoin":[String],
+  "Nexus":[String],
+  "PotCoin":[String],
+  "Xaurum":[String],
+  "Synereo":[String],
+  "Burst":[String],
+  "BitBay":[String],
+  "TokenCard":[String]
+}
 requestCurrencies.prototype.scheduleServer = function(bot){
-  schedule.scheduleJob('30 * * * *', Promise.coroutine(function* () {
+  schedule.scheduleJob('30 * * * * *', Promise.coroutine(function* () {
       let message ="Hello Joan last hour criptocurrency update:";
       let tempMessage="";
 
-      result = yield rp({url:"https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=15",method:'GET'});
+      result = yield rp({url:"https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=60",method:'GET'});
       result = JSON.parse(result)
       result.forEach(item =>{
-        console.log("24h " + item.name + " -> " + item.percent_change_24h + "%"+ " -- 1h " + item.percent_change_1h + " #price euro: " + item.price_eur);
-        tempMessage = "\n\n >24h " + item.name + " -> " + item.percent_change_24h + "%" + " -- 1h " + item.percent_change_1h + " #price euro: " + item.price_eur;
+      //  console.log("\"" + item.name +"\""  + ":[String],")
+        trend[item.name][0] = item.name.percent_change_24h;
+        console.log(trend[item.name] + " -- " + trend[item.name][0])
+        //console.log("24h " + item.name + " -> " + item.percent_change_24h + "%"+ " -- 1h " + item.percent_change_1h + " #price euro: " + item.price_eur);
+        tempMessage = "\n\n >24h " + item.name + " -> " + item.percent_change_24h + "%" + " -- 1h " + item.percent_change_1h + " #ppe: " + item.price_eur;
         message = message + tempMessage;
       })
 
