7346c40c4d7c249463557011c69a43c9ff18b6c9
Joan Perez <jperezes@cisco.com>
2018-01-05 11:32:31 +0100

    reenable bot

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
-//let port = process.env.PORT || 1337;
+let port = process.env.PORT || 1337;
 let sparklite = require("sparklite");
-//let botdomain = process.env.NUCKY_URL;
-//let sparkBot = new sparklite.SparkBot(process.env.NUCKY_KEY, port, botdomain);
+let botdomain = process.env.NUCKY_URL;
+let sparkBot = new sparklite.SparkBot(process.env.NUCKY_KEY, port, botdomain);
 let CheckCurrency = require('./routes/checkCripto');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
 
-// sparkBot.printHelloWorld();
+sparkBot.printHelloWorld();
 //
 let checkCurrency = new CheckCurrency()
-// sparkBot.on('message', function (event)
-// {
-//    var mail = event.personEmail.split('@');
-//    console.log("message received + :" + JSON.stringify(event))
-//    if (mail[1] === "cisco.com"){
-//
-//      let message = "your room id is : " + event.roomId;
-//      sparkBot.sendMessage(event.roomId, message ,function(){});
-//    } else {
-//
-//      sparkBot.sendMessage(event.roomId, "Hi, sorry to tell you that but you're not allowed to proceed",function(){});
-//    }
-//   console.log(JSON.stringify(event));
-//
-// })
-// sparkBot.on('rooms', function (event)
-// {
-//     console.log(JSON.stringify(event));
-// })
-//
-// sparkBot.on('memberships', function (event)
-// {
-//     console.log(JSON.stringify(event));
-// })
+sparkBot.on('message', function (event)
+{
+   var mail = event.personEmail.split('@');
+   console.log("message received + :" + JSON.stringify(event))
+   if (mail[1] === "cisco.com"){
+
+     let message = "your room id is : " + event.roomId;
+     sparkBot.sendMessage(event.roomId, message ,function(){});
+   } else {
+
+     sparkBot.sendMessage(event.roomId, "Hi, sorry to tell you that but you're not allowed to proceed",function(){});
+   }
+  console.log(JSON.stringify(event));
+
+})
+sparkBot.on('rooms', function (event)
+{
+    console.log(JSON.stringify(event));
+})
+
+sparkBot.on('memberships', function (event)
+{
+    console.log(JSON.stringify(event));
+})
 
 
 let bot = function(result){
